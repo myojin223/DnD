8d2273763dfe91938f669a06bdcfcf76de8a1e37
myojin223 <64243555+myojin223@users.noreply.github.com>
2022-01-09 08:06:47 -0700

    Initial upload

---
+++ DnD/DnD.bat
@@ -0,0 +1,1 @@
+java -jar proximity.jar --cards=Decklist.txt --art_source=BEST --reminder_text=false --template=DnD --copyright=false --use_card_back=false --watermark=NONE --set_symbol=pz1 --flavor_text=false
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# DnD
- DnD style template for Proximity 0.5.x
+# Drag and drop the folder inside of the "DnD-main" folder into your existing Proximity template directory
+
+## For Proximity 0.5.4+
